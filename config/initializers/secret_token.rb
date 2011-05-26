f53b0750804cabd17b4059b6bf0260e7a6fb8c1a
Mike Wille <mike.wille@flowz.com>
2011-05-26 14:49:03 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedCarpet::Application.config.secret_token = 'e3861af1a60af73bddad690c78ee9b87d11a6d560ed15f051d37157c2fe504b017c2d5b58f88871862bcb6f7020d2aac7e17a6600e25f0cd53b5c12bd2d9345a'
